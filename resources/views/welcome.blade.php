993907377a25f7c7b33e80e538fd9fd4034ccbb9
jacobmo92 <morris.jacob6@gmail.com>
2017-03-04 12:51:05 -0500

    Header finished. Beginning of green section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Dare or Dare</title>
 
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body class="home" id="home">
         <div class="container-fluid header">
-            <div class="col-xs-6 logo">DARE or Dare</div>
+            <div class="col-xs-12 logo">Dare or Dare</div>
         </div>
 
-        <div class="container">
-          <div class="text-center">
-            <h1>Content</h1>
-          </div>
-        </div><!-- /.container -->
+        <div class="container-fluid green section">
+            <div class="row">
+                <div class="col-xs-12 cards">
+                    <div class="col-xs-12 col-sm-4 card">
+                        <img src="{{ asset('/images/2-back.png') }}">
+                    </div>
+                    <div class="col-xs-12 col-sm-4 card">
+                        <img src="{{ asset('/images/1-back.png') }}">
+                    </div>
+                    <div class="col-xs-12 col-sm-4 card">
+                        <img src="{{ asset('/images/3-back.png') }}">
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
