08cc6206dd3180f293b051a1f3e0bec7b538856d
jacobmo92 <morris.jacob6@gmail.com>
2017-03-04 11:08:20 -0500

    Reupload initial. Upgraded to laravel 5.4 and added HTML support

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css">
+    </head>
+    <body class="home" id="home">
+        <div class="container-fluid header">
+            <div class="col-xs-6 logo">DARE or Dare</div>
+        </div>
+
+        <div class="container">
+          <div class="text-center">
+            <h1>Content</h1>
+          </div>
+        </div><!-- /.container -->
+    </body>
+</html>
